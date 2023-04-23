1dde9916908caaf336d3ca7f1a62f622cd5ec2ad
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-23 10:25:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+// let count = 0;
+let input = document.getElementById("evaluatedText");
+let display = document.getElementById("letterCount");
+input.addEventListener('input',myFunc);
+
+function myFunc(){
+	let text = document.getElementById("evaluatedText").value;
+	display.innerText = text.length;
+}
